483e8d8d08addda460928c428217777239a013b6
CodeMasterZeroOne <pawel.ict.student@gmail.com>
2020-02-06 16:17:31 +0000

    changed fromm  component to context

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class ToDoList extends Component {
-    static contextType = ThemeContext;
+////use of component example(import Component to use it)
+// class ToDoList extends Component {
+//     static contextType = ThemeContext;
 
-    state = {}
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className="todo-list" style={{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>Code all nighgt</li>
-                    <li style={{ background: theme.ui }}>Sleep all day</li>
-                    <li style={{ background: theme.ui }}>Repeat all again...</li>
-                </ul>
-            </div>
-        );
-    }
+//     state = {}
+//     render() {
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
+
+//         return (
+//             <div className="todo-list" style={{ background: theme.bg, color: theme.syntax }}>
+//                 <ul>
+//                     <li style={{ background: theme.ui }}>Code all nighgt</li>
+//                     <li style={{ background: theme.ui }}>Sleep all day</li>
+//                     <li style={{ background: theme.ui }}>Repeat all again...</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+//use of context example
+const ToDoList = () => {
+    const { isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+
+    return (
+        <div className="todo-list" style={{ background: theme.bg, color: theme.syntax }}>
+            <ul>
+                <li style={{ background: theme.ui }}>Code all nighgt</li>
+                <li style={{ background: theme.ui }}>Sleep all day</li>
+                <li style={{ background: theme.ui }}>Repeat all again...</li>
+            </ul>
+        </div>
+    );
 }
 
 export default ToDoList;
